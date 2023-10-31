b5a137179cb8022ac188a9236c555966fceb9349
SafiullahMohammady <safikhan2018@gmail.com>
2023-10-31 17:44:40 +0430

    safiedits

---
+++ src/components/Safisrc/Safi.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+export const Safi = () => {
+  return (
+    <>
+    <div>
+    <div className=' pt-[30px] mt-[100px] flex    justify-center  ml-[10%]  w-[80%] bg-[#000000]'>
+      <div  className=''>
+         <p className=' text-[#00FF66]'>Categories</p>
+         <p className=' text-white text-[46px] font-[600]'>Enhance Your </p>
+         <p className=' text-white text-[46px] font-[600]' >Music Experience</p>
+       <img className=' mt-[10px]' src="src/components/Safisrc/Frame 601.png" alt="" />
+      <button className=' w-[200px] mt-[40px] rounded-[10px]  h-[60px] text-white text-[20px] bg-[#00FF66]'>Buy Now</button>
+    
+       </div>
+      
+     <div className=' flex  items-center justify-center'>
+      <img className=' absolute' src="src/components/Safisrc/Ellipse 23.png" alt="" />
+      <img src="src/components/Safisrc/Frame 694.png" alt="" />
+     </div>
+   
+   
+    </div>
+  
+  
+    <div  className="container  w-[80%] ml-[10%] mt-[50px]">
+    <div  class="num1">
+  
+  <img className=' float-right' src="src/components/Safisrc/attractive-woman-wearing-hat-posing-black-background 1.png" alt="" />
+  
+  <p className=' text-white  text-[30px] absolute ml-[20px] mt-[100px] font-[600]'>Women’s Collections</p>  
+   <p className=' text-[#FAFAFA] absolute ml-[20px] mt-[160px] text-[17px] font-[500]  w-[300px] '>Featured woman collections that give you another vibe.</p>
+    <p className=' text-white absolute ml-[20px] mt-[230px] font-[500]'>Shop Now</p>
+    </div>
+    <div  class="num2">
+    <p className=' text-white absolute font-[600] text-[25px] ml-[20px] mt-[100px]'>Perfume</p>
+  <p className=' text-white absolute font-[600] mt-[150px] ml-[20px]'>GUCCI INTENSE OUD EDP</p>
+  <p className=' text-white font-[600] mt-[185px] ml-[20px] absolute'>Shop Now</p>
+      <img className=' m-auto' src="src/components/Safisrc/Frame 706.png" alt="" />
+    </div>
+    <div  class="num3">
+   <p className=' text-white absolute font-[600] text-[25px] ml-[20px] mt-[100px]'>Speakers</p>
+  <p className=' text-white absolute font-[600] mt-[150px] ml-[20px]'>Amazon wireless speakers</p>
+  <p className=' text-white font-[600] mt-[185px] ml-[20px] absolute'>Shop Now</p>
+  <img className=' m-auto' src="src/components/Safisrc/69-694768_amazon-echo-png-clipart-transparent-amazon-echo-png 1.png" alt="" />
+    </div>
+    <div  class="num4">
+    <p className=' text-white ml-[20px]  font-[600] text-[30px]  mt-[340px] absolute'>PlayStation 5</p>
+    <p className=' text-white ml-[20px]  font-[600]  w-[300px]  mt-[400px] absolute'>Black and White version of the PS5 coming out on sale.</p>
+  <p className=' text-white ml-[20px] font-[600]  mt-[470px] absolute'>  Shop Now</p>
+    <img className=' m-auto' src="src/components/Safisrc/ps5-slim-goedkope-playstation_large 1.png" alt="" />
+    
+    </div>
+  </div>
+    
+    </div>
+    </>
+  )
+}
