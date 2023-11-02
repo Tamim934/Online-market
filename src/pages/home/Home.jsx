a6d6408b5fa07e0c0d049cb69c322ff36082a246
shahkat123 <shahkatabdusattorov37@gmail.com>
2023-11-02 07:56:19 +0500

    shavkat

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ProductDetalis from '../../components/Productsicons/ProductDetalis'
+import axios from 'axios';
 
 const Home = () => {
+  
+  const Api = "http://localhost:3000/products";
+  async function getData() {
+    try {
+      const { data } = await axios.get(Api);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  },[])
   return (
-    <div>Home</div>
+    <div>
+      <ProductDetalis/>
+    </div>
   )
 }
 
